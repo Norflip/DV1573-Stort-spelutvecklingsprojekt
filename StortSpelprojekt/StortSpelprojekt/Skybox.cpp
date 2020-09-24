a811f3949818a1043766cc26e908ebd6cf69861e
Pudzz <55652759+Pudzz@users.noreply.github.com>
2020-09-24 12:08:54 +0200

    Skybox class

--- StortSpelprojekt/StortSpelprojekt/Skybox.cpp
+++ StortSpelprojekt/StortSpelprojekt/Skybox.cpp
@@ -1,11 +1,19 @@
 #include "Skybox.h"
+#include "Skybox.h"
 
 Skybox::Skybox(Object* object)
 {
+	std::vector<Mesh> skybox = ZWEBLoader::LoadMeshes(ZWEBLoadType::NoAnimation, "Models/brickSphere.ZWEB", renderer->GetDevice());
+	Material newmat;
 
-	object = 
+	object->AddComponent<MeshComponent>(skybox[0], newmat);
 }
 
 Skybox::~Skybox()
 {
+}
+
+void Skybox::SetDiffuse(Object* object, dx::XMFLOAT4 diffuse)
+{
+			
 }
--- StortSpelprojekt/StortSpelprojekt/Skybox.h
+++ StortSpelprojekt/StortSpelprojekt/Skybox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ZWEBLoader.h"
+#include "Shader.h"
 
 class Skybox
 {
@@ -7,8 +8,20 @@ public:
 	Skybox(Object* object);
 	~Skybox();
 
+	void LoadDDS();
 
+	void SetDiffuse(dx::XMFLOAT4);
+	void SetAmbient(dx::XMFLOAT4);
+	void SetSpecular(dx::XMFLOAT4);
 
-private:
+	dx::XMFLOAT4 GetDiffuse() { return this->diffuse; }
+	dx::XMFLOAT4 GetAmbient() { return this->ambient; }
+	dx::XMFLOAT4 GetSpecular() { return this->specular; }
 
+private:	
+	Object* skybox;
+
+	dx::XMFLOAT4 diffuse;
+	dx::XMFLOAT4 ambient;
+	dx::XMFLOAT4 specular;
 };
