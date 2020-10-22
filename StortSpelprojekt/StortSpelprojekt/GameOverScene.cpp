32a993fdf2bdb117cfccd6b8058d1049bab8b8e9
Yoslo <fredrikgalfi@hotmail.com>
2020-10-22 12:32:09 +0200

    Jakov gnällde

--- StortSpelprojekt/StortSpelprojekt/GameOverScene.cpp
+++ StortSpelprojekt/StortSpelprojekt/GameOverScene.cpp
@@ -46,7 +46,7 @@ void GameOverScene::InitializeGUI()
 {
 	//spriteBatch = new DirectX::SpriteBatch(renderer->GetContext());
 	GUISprite* restart = new GUISprite(*renderer, "Textures/Restart.png", 100, 200, 0, DrawDirection::Default, ClickFunction::Clickable);
-	GUISprite* quit = new GUISprite(*renderer, "Textures/exit.png", 100, 400, 0, DrawDirection::Default, ClickFunction::Clickable);
+	GUISprite* quit = new GUISprite(*renderer, "Textures/Exit.png", 100, 400, 0, DrawDirection::Default, ClickFunction::Clickable);
 	GUIFont* fpsDisplay = new GUIFont(*renderer, "fps", windowWidth / 2, 50);
 	restart->SetActive();
 	quit->SetActive();
