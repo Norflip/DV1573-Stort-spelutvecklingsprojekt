521fd7ed7fcc97edcefe1098648e5212a7fc0b83
Norflip <Norgren.filip@gmail.com>
2020-10-28 10:43:59 +0100

    Update main.cpp

--- StortSpelprojekt/StortSpelprojekt/main.cpp
+++ StortSpelprojekt/StortSpelprojekt/main.cpp
@@ -2,14 +2,15 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
+#if _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // _DEBUG
 
 	// Use this to search for memory leak locations
 	//_crtBreakAlloc = 202216;
 
-	Engine engine(hInstance);	
-		
-	engine.Run();	
+	Engine engine(hInstance);
+	engine.Run();
 
 	return 0;
 }
