af378fc6f2dce489030b5e13df170189a94afcc7
Norflip <Norgren.filip@gmail.com>
2020-10-28 15:42:32 +0100

    Update GameScene.cpp

--- StortSpelprojekt/StortSpelprojekt/GameScene.cpp
+++ StortSpelprojekt/StortSpelprojekt/GameScene.cpp
@@ -215,7 +215,7 @@ void GameScene::InitializeObjects()
 
 //Axe//////////////////////////////////////////////////////////////////
 	std::vector<Mesh> axeMesh = ZWEBLoader::LoadMeshes(ZWEBLoadType::NoAnimation, "Models/AXE.ZWEB", renderer->GetDevice());
-	std::vector<Material> axeMat = ZWEBLoader::LoadMaterials("Models/AXE.ZWEB", defShader, renderer->GetDevice());
+	std::vector<Material> axeMat = ZWEBLoader::LoadMaterials("Models/AXE.ZWEB", defaultShader, renderer->GetDevice());
 
 	Object* axeObject = new Object("Axe");
 
