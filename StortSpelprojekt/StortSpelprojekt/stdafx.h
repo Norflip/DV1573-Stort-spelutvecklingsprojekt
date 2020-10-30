a20a43b0190fe346fe4f72246f4489915fedbe58
JAKOB LIDBORN <jala12@student.bth.se>
2020-10-30 14:55:08 +0100

    saf
    asf

--- StortSpelprojekt/StortSpelprojekt/stdafx.h
+++ StortSpelprojekt/StortSpelprojekt/stdafx.h
@@ -1,19 +1,7 @@
 #pragma once
 
 #define win32_lean_and_mean
-//GLOBAL DEFINES
-
-#define ALIGN16 __declspec(align(16))
-#define RELEASE(cptr) if(cptr) { cptr->Release(); cptr = nullptr; }
-
-#define FCAST(T) static_cast<float>(T)
-#define DCAST(T) static_cast<double>(T)
-#define ICAST(T) static_cast<int>(T)
-#define UICAST(T) static_cast<unsigned int>(T)
-
-#define MM_MALLOC_OPERATOR void* operator new(size_t i) { return _mm_malloc(i, 16); }
-#define MM_FREE_OPERATOR void operator delete(void* p){ _mm_free(p); }
-#define ALIGN16_ALLOC MM_MALLOC_OPERATOR MM_FREE_OPERATOR
+#include "Defines.h"
 //GLOBAL INCLUDES
 #include <react3d.h>
 #include <vector>
