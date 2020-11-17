035ac2e5ea65a1130e202686be498f77f67c9622
TBEW99 <49557882+TBEW99@users.noreply.github.com>
2020-11-17 10:20:42 +0100

    fixing a small thing

--- StortSpelprojekt/StortSpelprojekt/Renderer.cpp
+++ StortSpelprojekt/StortSpelprojekt/Renderer.cpp
@@ -140,18 +140,18 @@ void Renderer::DrawQueueToTarget(RenderQueue& queue, CameraComponent* camera)
 void Renderer::RenderFrame(CameraComponent* camera, float time)
 {
 	// UPDATE SCENE
-	if (KEY_PRESSED(P) && isFullScreen == true)
+	if (KEY_PRESSED(Y) && isFullScreen == true)
 	{
 		isFullScreen = false;
 	}
-	else if (KEY_PRESSED(P) && isFullScreen == false)
+	else if (KEY_PRESSED(Y) && isFullScreen == false)
 	{
 		isFullScreen = true;
 	}
 	RenderFrame(camera, time, backbuffer, true, true);
 	HRESULT hr = swapchain->Present(0, 0); //1 here?
-	//swapchain->SetFullscreenState(isFullScreen, nullptr);
-	swapchain->SetFullscreenState(true, nullptr);
+	swapchain->SetFullscreenState(isFullScreen, nullptr);
+	//swapchain->SetFullscreenState(true, nullptr);
 	assert(SUCCEEDED(hr));
 }
 
