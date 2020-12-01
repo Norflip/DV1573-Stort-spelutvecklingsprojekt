c3ac5f404e2e00c188419f74c5dc504aeed2e25a
Pudzz <55652759+Pudzz@users.noreply.github.com>
2020-12-01 22:54:33 +0100

    bortkommenterade saker

--- StortSpelprojekt/StortSpelprojekt/GameScene.cpp
+++ StortSpelprojekt/StortSpelprojekt/GameScene.cpp
@@ -139,39 +139,39 @@ void GameScene::InitializeObjects()
 
 	world.Initialize(root, resources, renderer);
 
-	/* PICKUP STUFF DONT DELETE THESEEE */
-	Object* healthkitObject = resources->AssembleObject("HealthKit", "HealthKitMaterial");
-	healthkitObject->AddFlag(ObjectFlag::DEFAULT);
-	healthkitObject->GetComponent<MeshComponent>()->SetBatchable(true);
-	healthkitObject->GetTransform().SetPosition({ 23,2,50 });
-	healthkitObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.5f, 0.5f, 0.5f }, dx::XMFLOAT3{ 0, 0, 0 });
-	healthkitObject->AddComponent<PickupComponent>(PickupType::Health, 20.0f);
-	healthkitObject->AddComponent<RigidBodyComponent>(0.f, FilterGroups::PICKUPS, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
-	AddObjectToRoot(healthkitObject);
+	///* PICKUP STUFF DONT DELETE THESEEE */
+	//Object* healthkitObject = resources->AssembleObject("HealthKit", "HealthKitMaterial");
+	//healthkitObject->AddFlag(ObjectFlag::DEFAULT);
+	//healthkitObject->GetComponent<MeshComponent>()->SetBatchable(true);
+	//healthkitObject->GetTransform().SetPosition({ 23,2,50 });
+	//healthkitObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.5f, 0.5f, 0.5f }, dx::XMFLOAT3{ 0, 0, 0 });
+	//healthkitObject->AddComponent<PickupComponent>(PickupType::Health, 20.0f);
+	//healthkitObject->AddComponent<RigidBodyComponent>(0.f, FilterGroups::PICKUPS, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
+	//AddObjectToRoot(healthkitObject);
 
-	///* Fuel pickup stuff temporary */
-	Object* fuelCanObject = resources->AssembleObject("FuelCanGreen", "FuelCanGreenMaterial");
-	fuelCanObject->AddFlag(ObjectFlag::DEFAULT);
-	fuelCanObject->GetComponent<MeshComponent>()->SetBatchable(true);
-	fuelCanObject->GetTransform().SetPosition({ 22,2,52 });
-	fuelCanObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.3f, 0.3f, 0.3f }, dx::XMFLOAT3{ 0, 0, 0 });
-	fuelCanObject->AddComponent<PickupComponent>(PickupType::Fuel, 20.0f);
-	fuelCanObject->AddComponent<RigidBodyComponent>(10.f, FilterGroups::HOLDABLE, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
-	AddObjectToRoot(fuelCanObject);
+	/////* Fuel pickup stuff temporary */
+	//Object* fuelCanObject = resources->AssembleObject("FuelCanGreen", "FuelCanGreenMaterial");
+	//fuelCanObject->AddFlag(ObjectFlag::DEFAULT);
+	//fuelCanObject->GetComponent<MeshComponent>()->SetBatchable(true);
+	//fuelCanObject->GetTransform().SetPosition({ 22,2,52 });
+	//fuelCanObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.3f, 0.3f, 0.3f }, dx::XMFLOAT3{ 0, 0, 0 });
+	//fuelCanObject->AddComponent<PickupComponent>(PickupType::Fuel, 20.0f);
+	//fuelCanObject->AddComponent<RigidBodyComponent>(10.f, FilterGroups::HOLDABLE, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
+	//AddObjectToRoot(fuelCanObject);
 
-	///* Banana pickup stuff temporary */
-	Shader* particleShader = resources->GetShaderResource("particleShader");
-	Object* beansObject = resources->AssembleObject("Soup", "SoupMaterial");
-	beansObject->AddFlag(ObjectFlag::DEFAULT);
-	beansObject->GetComponent<MeshComponent>()->SetBatchable(true);
-	beansObject->GetTransform().SetPosition({ 22, 2.0f, 53 });
-	beansObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.5f, 0.5f, 0.5f }, dx::XMFLOAT3{ 0, 0, 0 });
-	beansObject->AddComponent<PickupComponent>(PickupType::Food, 20.0f);
-	beansObject->AddComponent<RigidBodyComponent>(0.f, FilterGroups::PICKUPS, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
+	/////* Banana pickup stuff temporary */
+	//Shader* particleShader = resources->GetShaderResource("particleShader");
+	//Object* beansObject = resources->AssembleObject("Soup", "SoupMaterial");
+	//beansObject->AddFlag(ObjectFlag::DEFAULT);
+	//beansObject->GetComponent<MeshComponent>()->SetBatchable(true);
+	//beansObject->GetTransform().SetPosition({ 22, 2.0f, 53 });
+	//beansObject->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 0.5f, 0.5f, 0.5f }, dx::XMFLOAT3{ 0, 0, 0 });
+	//beansObject->AddComponent<PickupComponent>(PickupType::Food, 20.0f);
+	//beansObject->AddComponent<RigidBodyComponent>(0.f, FilterGroups::PICKUPS, (FilterGroups::EVERYTHING & ~FilterGroups::PLAYER), BodyType::DYNAMIC, true);
 
-	beansObject->AddComponent<ParticleSystemComponent>(renderer, particleShader);
-	beansObject->GetComponent<ParticleSystemComponent>()->InitializeParticles(renderer->GetDevice(), "Stars");
-	AddObjectToRoot(beansObject);
+	//beansObject->AddComponent<ParticleSystemComponent>(renderer, particleShader);
+	//beansObject->GetComponent<ParticleSystemComponent>()->InitializeParticles(renderer->GetDevice(), "Stars");
+	//AddObjectToRoot(beansObject);
 
 	//Player Arms
 	Object* playerArms = new Object("PlayerArms", ObjectFlag::DEFAULT | ObjectFlag::NO_CULL);
@@ -221,7 +221,7 @@ void GameScene::InitializeObjects()
 	//puzzleFrog->AddComponent<RigidBodyComponent>(50.f, FilterGroups::PROPS, (FilterGroups::EVERYTHING), BodyType::DYNAMIC, true);*/
 	//AddObject(puzzleFrog);
 
-	Object* puzzleFly = resources->AssembleObject("PuzzleFlyStatue", "PuzzleFlyStatueMaterial", false, ObjectFlag::DEFAULT | ObjectFlag::NO_CULL);
+	/*Object* puzzleFly = resources->AssembleObject("PuzzleFlyStatue", "PuzzleFlyStatueMaterial", false, ObjectFlag::DEFAULT | ObjectFlag::NO_CULL);
 	puzzleFly->GetTransform().SetPosition({ 28, 1.3f, 50 });
 	puzzleFly->AddComponent<BoxColliderComponent>(dx::XMFLOAT3{ 1.0f, 1.0f, 1.0f }, dx::XMFLOAT3{ 0, 0, 0 });
 	puzzleFly->AddComponent<RigidBodyComponent>(0.0f, FilterGroups::HOLDABLE, FilterGroups::EVERYTHING, BodyType::DYNAMIC, true);
@@ -229,7 +229,7 @@ void GameScene::InitializeObjects()
 	puzzleFly->GetComponent<ParticleSystemComponent>()->SetMaxParticles(50);
 	puzzleFly->GetComponent<ParticleSystemComponent>()->SetParticleSize(0.1f);
 	puzzleFly->GetComponent<ParticleSystemComponent>()->InitializeParticles(renderer->GetDevice(), "Fire1");
-	AddObjectToRoot(puzzleFly);
+	AddObjectToRoot(puzzleFly);*/
 
 	/*Shader* fireShader = resources->GetShaderResource("fireShader");*/
 
