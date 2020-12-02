6f56cd84f3c0dc8d0c016a8eebaab2dbdb225810
Norflip <Norgren.filip@gmail.com>
2020-12-02 20:09:04 +0100

    Update GameScene.cpp

--- StortSpelprojekt/StortSpelprojekt/GameScene.cpp
+++ StortSpelprojekt/StortSpelprojekt/GameScene.cpp
@@ -738,6 +738,12 @@ void GameScene::Update(const float& deltaTime)
 	if (rightSign->GetComponent<SelectableComponent>()->GetActive())
 	{
 		//set first frame till false
+
+		SaveState state = SaveHandler::LoadOrCreate();
+		state.segment++;
+		SaveHandler::Save(state);
+		std::cout << "added +1 to segment in save" << std::endl;
+
 		OnDeactivate();
 		ShowCursor(false);
 		OnActivate();
@@ -745,6 +751,12 @@ void GameScene::Update(const float& deltaTime)
 	}
 	else if (leftSign->GetComponent<SelectableComponent>()->GetActive())
 	{
+		SaveState state = SaveHandler::LoadOrCreate();
+		state.segment++;
+		SaveHandler::Save(state);
+
+		std::cout << "added +1 to segment in save" << std::endl;
+
 		OnDeactivate();
 		ShowCursor(false);
 		OnActivate();
