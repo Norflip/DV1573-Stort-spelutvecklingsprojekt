e235a546e94e2f4b4d8a542e1b8afb1010857208
TBEW99 <49557882+TBEW99@users.noreply.github.com>
2020-12-07 10:40:37 +0100

    ...

--- StortSpelprojekt/StortSpelprojekt/Shaders/Emissive_ps.hlsl
+++ StortSpelprojekt/StortSpelprojekt/Shaders/Emissive_ps.hlsl
@@ -5,7 +5,11 @@ SamplerState emissiveSampler;
 
 struct Emissive_PS_INPUT
 {
+    float4 position : SV_POSITION;
+    float3 normal : NORMAL;
     float2 uv : TEXCOORD0;
+    float3 worldPosition : POSITION;
+    float3 tangent : TANGENT;
 };
 
 float4 main(VS_OUTPUT input) : SV_TARGET
