3ec523f7ea15ba2052aebece39e94c03536eaad6
JAKOB LIDBORN <jala12@student.bth.se>
2020-12-17 14:57:53 +0100

    a
    Fixed so house just needs to be near end

--- StortSpelprojekt/StortSpelprojekt/PlayerComp.cpp
+++ StortSpelprojekt/StortSpelprojekt/PlayerComp.cpp
@@ -433,7 +433,7 @@ void PlayerComp::RayCast(const float& deltaTime)
 
 	physics->RaytestSingle(ray, rayDistance, hit, FilterGroups::CLICKABLE);
 	//Click signs
-	if (KEY_DOWN(E) && hit.object != nullptr && house->GetComponent<NodeWalkerComp>()->GetHouseProgress()==1.f)
+	if (KEY_DOWN(E) && hit.object != nullptr && house->GetComponent<NodeWalkerComp>()->GetHouseProgress()>0.9f)
 	{
 		clickable = hit.object;
 		AudioMaster::Instance().PlaySoundEvent("punch");
