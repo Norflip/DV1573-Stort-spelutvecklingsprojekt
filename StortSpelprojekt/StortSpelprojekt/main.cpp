1100de3bafa5403f8d559e88bda29d62bbe3af6c
Norflip <Norgren.filip@gmail.com>
2020-12-17 08:46:32 +0100

    leaks n player stat reset fix

--- StortSpelprojekt/StortSpelprojekt/main.cpp
+++ StortSpelprojekt/StortSpelprojekt/main.cpp
@@ -3,9 +3,7 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-#if _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif // _DEBUG
 
 	// Use this to search for memory leak locations
 	//_crtBreakAlloc = 2268518;
